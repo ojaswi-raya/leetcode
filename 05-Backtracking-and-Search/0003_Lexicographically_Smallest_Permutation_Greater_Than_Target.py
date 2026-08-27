27887b7bff8f1227deb74862a67da8d4c29fbcb2
Ojaswi <ojaswirayamajhi3@gmail.com>
2026-08-27 19:52:35 +0200

    debug

--- 05-Backtracking-and-Search/0003_Lexicographically_Smallest_Permutation_Greater_Than_Target.py
+++ 05-Backtracking-and-Search/0003_Lexicographically_Smallest_Permutation_Greater_Than_Target.py
@@ -3,45 +3,48 @@ from collections import Counter
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
         n = len(s)
-        total_counts = Counter(s)
+        s_counts = Counter(s)
         
-        best_i = -1
-        best_char = ''
-        
-        # Identify the rightmost position `i` where we can pick a character > target[i]
+        # Determine the longest prefix of target that s can match
+        pref_len = 0
+        temp_counts = s_counts.copy()
         for i in range(n):
-            char_t = target[i]
-            
-            # Look for a character larger than target[i]
-            for c in sorted(total_counts.keys()):
-                if c > char_t and total_counts[c] > 0:
-                    best_i = i
-                    best_char = c
-            
-            # Extend prefix match if target[i] is available
-            if total_counts[char_t] > 0:
-                total_counts[char_t] -= 1
+            if temp_counts[target[i]] > 0:
+                temp_counts[target[i]] -= 1
+                pref_len += 1
             else:
                 break
-        
-        if best_i == -1:
-            return ""
-        
-        # Reconstruct the string
-        counts = Counter(s)
-        res = []
-        
-        # 1. Matching prefix [0 ... best_i - 1]
-        for i in range(best_i):
-            res.append(target[i])
-            counts[target[i]] -= 1
+
+        # Check positions from rightmost possible matching prefix down to 0
+        for i in range(pref_len, -1, -1):
+            if i == n:
+                # Equal length match means s can form target exactly,
+                # but we need strictly greater, so backtrack to i = n - 1
+                continue
+                
+            # Compute remaining available characters after matching target[0...i-1]
+            avail = s_counts.copy()
+            for j in range(i):
+                avail[target[j]] -= 1
             
-        # 2. Place smallest character larger than target[best_i]
-        res.append(best_char)
-        counts[best_char] -= 1
-        
-        # 3. Fill suffix with remaining available characters in sorted order
-        for c in sorted(counts.keys()):
-            res.append(c * counts[c])
+            # Find the smallest character strictly greater than target[i]
+            target_char = target[i]
+            next_char = None
+            for c in sorted(avail.keys()):
+                if c > target_char and avail[c] > 0:
+                    next_char = c
+                    break
             
-        return "".join(res)
+            if next_char:
+                # Construct result: target[0...i-1] + next_char + sorted remaining chars
+                res = list(target[:i])
+                res.append(next_char)
+                avail[next_char] -= 1
+                
+                for c in sorted(avail.keys()):
+                    if avail[c] > 0:
+                        res.append(c * avail[c])
+                
+                return "".join(res)
+                
+        return ""
