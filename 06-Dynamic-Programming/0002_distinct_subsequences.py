7b39b2729065391c31c78c891ea23012377d3bf9
Ojaswi <ojaswirayamajhi3@gmail.com>
2026-09-06 21:38:37 +0200

    distinct subsequences.py

---
+++ 06-Dynamic-Programming/0002_distinct_subsequences.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        
+        # dp[i][j] stores the number of distinct subsequences 
+        # of s[0...i-1] that equal t[0...j-1]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        # Base case: empty string t can be formed by 1 way (deleting all chars of s)
+        for i in range(m + 1):
+            dp[i][0] = 1
+            
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+                    
+        return dp[m][n]
